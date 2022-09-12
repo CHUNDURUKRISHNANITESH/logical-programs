70116fcbf5675d7d4c1794e5a50b0b4b697772c2
krishna nitesh <67718188+CHUNDURUKRISHNANITESH@users.noreply.github.com>
2022-09-12 14:17:55 +0530

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,24 @@
+//Write a Java program to convert a List/ArrayList(with duplicate elements) into HashSet containing no duplicates.
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Iterator;
+class Exercise1 {
+  public static void main(String[] args){
+    ArrayList<String> a=new ArrayList<String>();
+    a.add("zebra");
+    a.add("tiger");
+    a.add("lion");
+    a.add("elephant");
+    a.add("horse");
+    a.add("zebra");
+    a.add("lion");
+    System.out.println("ArrayList with duplicates: \n"+a);
+    Iterator i=a.iterator();
+    HashSet<String> h=new HashSet<String>();
+    while(i.hasNext()){
+      String s=(String)i.next();
+      h.add(s);
+     }
+    System.out.println("\n HashSet without duplicates: \n"+h);
+  }
+}
