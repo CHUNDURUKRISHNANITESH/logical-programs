c967c52c158f50b85b3e0984a7933591dd3e2706
krishna nitesh <67718188+CHUNDURUKRISHNANITESH@users.noreply.github.com>
2022-09-12 14:11:38 +0530

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,13 @@
+// Write a program to print your Name, Father's Name and Mailing Address in different lines
+
+class Exercise1 {
+
+  public static void main(String[] args){
+    System.out.println("Krishna nitesh ");
+    System.out.println("Suresh kumar");
+    System.out.println("nitesh.chunduru@gmail.com");
+  }
+
+
+
+} 
