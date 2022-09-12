8404403b3808a5f6fd183dc6a21ad95e19049a68
krishna nitesh <67718188+CHUNDURUKRISHNANITESH@users.noreply.github.com>
2022-09-12 14:20:28 +0530

    Add files via upload

---
+++ OutOfMemoryError_Exercise.java
@@ -0,0 +1,13 @@
+/* 4. Write a program to add Random values into HashMap using infinite loop. To get OutOfMemoryError, 
+   create HashMap class and use Random class to add random values into HashMap.
+*/
+import java.util.*;
+public class OutOfMemoryError_Exercise {
+
+    public static void main(String args[]) {
+      HashMap hm=new HashMap();
+      Random r=new Random();
+      while(true)
+         hm.put(r.nextInt(),r.nextInt());
+    }
+}
---
+++ StackOverFlowError_Exercise.java
@@ -0,0 +1,20 @@
+/* 5.Write a program to find factorial of a given number using recursive function. 
+   Do not provide termination condition to get StackOverFlowError.
+*/
+
+public class StackOverFlowError_Exercise {
+
+  public void factorial(int num)
+  {
+    int fact=1,n;
+    n=num;
+    while(true){
+      fact=fact*n;
+      n--;
+      factorial(n);
+    }
+  }
+  public static void main(String args[]) {
+    new StackOverFlowError_Exercise().factorial(5);
+    }
+}
